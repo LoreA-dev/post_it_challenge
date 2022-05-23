2167b6b8e239162e8a13184fab11bb1a131d6bf5
LoreA-dev <ruby-loren@hotmail.com>
2022-05-22 20:47:15 -0500

    Trash function

--- src/DataNote/index.js
+++ src/DataNote/index.js
@@ -6,9 +6,13 @@ function DataNote({id, text, positionX, positionY,updatePosition, onDelete}){
     const [editable, setEditable] = React.useState(true)
     const [saveInformation, setSaveInformation] = React.useState("");
     
+    const onDragElement = (evt)=>{
+        evt.dataTransfer.setData("myid", id);
+    }
+
     return(
-        <div draggable="true" onDragEnd={(evt) => updatePosition(id,evt)} className="newPostIt" id={"note_" + id} onDoubleClick ={(evt)=>{evt.stopPropagation();}} style={{top: positionY, left: positionX}}>
-          <button id="deletePostItButton" onClick={onDelete} ><i className="fas fa-times"></i></button>
+        <div onDragStart={onDragElement} draggable="true" onDragEnd={(evt) => updatePosition(id,evt)} className="newPostIt" id={"note_" + id} onDoubleClick ={(evt)=>{evt.stopPropagation();}} style={{top: positionY, left: positionX}}>
+          <button className="deletePostItButton" onClick={onDelete} ><i className="fas fa-times"></i></button>
           { !editable ? <p className="pText" >{text}</p>: <textarea onChange={(evt)=> setSaveInformation(evt.target.defaultValue)} maxLength="70" autoCapitalize="sentences" className="textBox" onKeyDown={(e)=>{
               if(e.code === 'Enter'){
                   setEditable(!editable)
